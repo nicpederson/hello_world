957c4ebe91fcc7ddf3046a21d40486997ca4d494
nicpederson <nicpederson@gmail.com>
2021-02-11 18:07:14 -0800

    s

--- hello.php
+++ hello.php
@@ -6,7 +6,7 @@
 * GitHub Plugin URI: https://github.com/nicpederson/hello_world
 * Primary Branch: main2
 * Description: This plugin does some stuff with WordPress
-* Version: 1.0.3
+* Version: 1.0.4
 * Author: Nic
 * Author URI: https://nicpederson.com
 * License: GPL2
