e7027548524fcccf8609e9241bacf8b7c3394397
nicpederson <nicpederson@gmail.com>
2021-02-11 16:56:06 -0800

    update

--- hello.php
+++ hello.php
@@ -3,6 +3,7 @@
 /**
 * Plugin Name: Test plugin
 * Plugin URI: https://nicpederson.com
+* GitHub Plugin URI: https://github.com/nicpederson/hello_world
 * Description: This plugin does some stuff with WordPress
 * Version: 1.0.0
 * Author: Nic
